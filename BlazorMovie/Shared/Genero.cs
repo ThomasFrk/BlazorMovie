566542e860b2e00b8c4f215d7279a370360ba15d
Thomas <thomasfrank2008@hotmail.com>
2020-09-10 18:09:37 -0300

    first commit

---
+++ BlazorMovie/Shared/Genero.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+
+
+namespace BlazorMovie.Shared
+{
+    public class Genero
+    {
+
+        public int Id { get; set; }
+
+
+        [Required(ErrorMessage = "Nome é obrigatório")]
+        public string Name { get; set; }
+
+    }
+}
---
+++ BlazorMovie/Shared/Usuario.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace BlazorMovie.Shared
+{
+    public class Usuario
+    {
+        [Required]
+        public string Titulo { get; set; }
+
+        [Required]
+        public string Nome { get; set; }
+
+        [Required]
+        public string Sobrenome { get; set; }
+
+        [Required]
+        public DateTime DataNascimento { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [MinLength(6,ErrorMessage ="Senha deve ter 6 ou mais caracteres")]
+        public string Senha { get; set; }
+
+        [Required]
+        [Compare("Senha", ErrorMessage ="Senhas não batem")]
+        //nao consegui fazer o CompareProperty porque fala que o comando nao existe, mesmo baixando o pacote experimental
+        public string ConfirmarSenha { get; set; }
+
+        [Required]
+        [Range(typeof(bool), "true", "true", ErrorMessage = "Aceitar os termos de serviço")]
+        public bool TermosServ { get; set; }
+    }
+}
